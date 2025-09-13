7ad497ea7846eedc86c2928deb3bb74c28dc146b
Atqa Munzir <atqamz@gmail.com>
2025-09-13 15:26:42 +0700

    fix API_URL

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  `${process.env.NEXT_PUBLIC_API_URL}/api/v1` || "http://localhost:8080/api/v1";
+  `${process.env.NEXT_PUBLIC_API_URL}` || "http://localhost:8080/api/v1";
 
 const apiClient = axios.create({
   baseURL: API_URL,
